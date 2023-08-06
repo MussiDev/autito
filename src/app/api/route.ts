bc263bb6364af8ba3655a5540719e5e8c8c55e59
mussiDev <joakoomussi@gmail.com>
2023-08-06 13:57:36 -0300

    delete route

--- src/app/api/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from 'next/server';
-import { connectionDB } from '@/src/utils/connection/mongoose';
-
-export const GET = () => {
-    connectionDB()
-    return NextResponse.json({
-        message: 'Done'
-    })
-}

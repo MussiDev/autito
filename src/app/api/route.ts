ea9086bfacedcc1325388461c44540a59dd0af46
mussiDev <joakoomussi@gmail.com>
2023-08-06 13:56:42 -0300

    create connection to db

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,6 +1,8 @@
 import { NextResponse } from 'next/server';
+import { connectionDB } from '@/src/utils/connection/mongoose';
 
 export const GET = () => {
+    connectionDB()
     return NextResponse.json({
         message: 'Done'
     })

2755ae3069f4b187ec95cee3968475912bb98e05
mussiDev <joakoomussi@gmail.com>
2023-08-21 18:53:20 -0300

    refactor form

--- src/app/travels/newTravel/page.tsx
+++ src/app/travels/newTravel/page.tsx
@@ -1,7 +1,16 @@
 import Form from "@/src/components/Form/Form";
 import { Suspense } from "react";
 
-export default function NewTravel() {
+async function loadProvinceAndCity() {
+    const response = await fetch("https://apis.datos.gob.ar/georef/api/provincias");
+    if (!response.ok) {
+        throw new Error("Failed to fetch provinces");
+    }
+    const provinces = await response.json();
+    return provinces;
+}
+export default async function NewTravel() {
+    const provinces = await loadProvinceAndCity();
     return (
         <Suspense fallback={<p>Loading...</p>}>
             <Form />
--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -4,21 +4,15 @@ import React, { ChangeEvent, FormEvent, useEffect, useState } from "react";
 import { useParams, useRouter } from "next/navigation";
 
 import Button from "../../common/Button/Button";
-import Dropdown from "@/src/common/Dropdown/Dropdown";
 import { Travel } from "@/entities/Travels/Travel";
+
+type FieldName = keyof Travel;
 
 export default function Form() {
     const router = useRouter();
-    const params = useParams();
-    const [newTravel, setNewTravel] = useState<Travel>({
-        destiny: "",
-        ubication: "",
-        date: new Date(),
-        hour: "",
-        places: 4,
-        description: "",
-    });
-    const [editTravel, setEditTravel] = useState<Travel>({
+    const { id } = useParams();
+
+    const getInitialTravel = (): Travel => ({
         destiny: "",
         ubication: "",
         date: new Date(),
@@ -27,8 +21,20 @@ export default function Form() {
         description: "",
     });
 
+    const [newTravel, setNewTravel] = useState<Travel>(getInitialTravel());
+    const [editTravel, setEditTravel] = useState<Travel>(getInitialTravel());
+
+    const [selectDestiny, setSelectDestiny] = useState<string | null>(null);
+    const [selectUbication, setSelectUbication] = useState<string | null>(null);
+
+    useEffect(() => {
+        if (id) {
+            getTravel();
+        }
+    }, []);
+
     const getTravel = async () => {
-        const res = await fetch(`/api/travels/${params.id}`);
+        const res = await fetch(`/api/travels/${id}`);
         const data = await res.json();
 
         setEditTravel({
@@ -41,28 +47,39 @@ export default function Form() {
         });
     };
 
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        if (id) {
+            setEditTravel({ ...editTravel, [e.target.name]: e.target.value });
+        }
+        setNewTravel({ ...newTravel, [e.target.name]: e.target.value });
+    };
+
     const handleSubmit = async (e: FormEvent) => {
         e.preventDefault();
-        const { destiny, ubication, date, hour, places } = !params.id ? newTravel : editTravel;
+        const travel = id ? editTravel : newTravel;
 
-        if (!destiny || !ubication || !date || !hour || !places) return;
+        if (!travel.destiny || !travel.ubication || !travel.date || !travel.hour || !travel.places) return;
 
         try {
-            const response = !params.id
-                ? await fetch("/api/travels", {
-                      method: "POST",
-                      headers: {
-                          "Content-Type": "application/json",
-                      },
-                      body: JSON.stringify(newTravel),
-                  })
-                : await fetch(`/api/travels/${params.id}`, {
-                      method: "PUT",
-                      headers: {
-                          "Content-type": "application/json",
-                      },
-                      body: JSON.stringify(editTravel),
-                  });
+            let response;
+            if (id) {
+                response = await fetch(`/api/travels/${id}`, {
+                    method: "PUT",
+                    headers: {
+                        "Content-type": "application/json",
+                    },
+                    body: JSON.stringify(travel),
+                });
+            } else {
+                response = await fetch("/api/travels", {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify(travel),
+                });
+            }
+
             if (response.ok) {
                 router.push("/");
                 router.refresh();
@@ -72,101 +89,35 @@ export default function Form() {
         }
     };
 
-    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-        if (params.id) {
-            setEditTravel({ ...editTravel, [e.target.name]: e.target.value });
-        }
-        setNewTravel({ ...newTravel, [e.target.name]: e.target.value });
-    };
-
-    useEffect(() => {
-        if (params.id) {
-            getTravel();
-        }
-    }, []);
+    const inputFields = [
+        { label: "Destino", name: "destiny", type: "text" },
+        { label: "Ubicación", name: "ubication", type: "text" },
+        { label: "Fecha", name: "date", type: "date" },
+        { label: "Hora", name: "hour", type: "time" },
+        { label: "Lugares", name: "places", type: "text" },
+        { label: "Descripción", name: "description", type: "text" },
+    ];
 
     return (
         <div className="w-full max-w-sm">
             <form className="bg-main-900 shadow-md rounded px-8 pt-6 pb-8 mb-4 text-orange-500" onSubmit={handleSubmit}>
-                <div className="mb-4">
-                    <label className="block text-sm font-bold mb-2" htmlFor="destino">
-                        Destino
-                    </label>
-                    <Dropdown data={{ text: "Elija el destino" }} />
-                </div>
-                <div className="mb-4">
-                    <label className="block text-sm font-bold mb-2" htmlFor="ubicación">
-                        Ubicación
-                    </label>
-                    <Dropdown data={{ text: "Elija la ubicación" }} />
-                </div>
-                <div className="mb-4">
-                    <label className="block text-sm font-bold mb-2" htmlFor="fecha">
-                        Fecha
-                    </label>
-                    <input
-                        className="bg-main-900 text-white shadow appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
-                        id="date"
-                        name="date"
-                        type="Date"
-                        placeholder="Fecha"
-                        onChange={handleChange}
-                        value={
-                            !params.id
-                                ? newTravel.date.toISOString().substring(0, 10)
-                                : editTravel?.date instanceof Date
-                                ? editTravel.date.toISOString().substring(0, 10)
-                                : ""
-                        }
-                    />
-                </div>
-                <div className="mb-4">
-                    <label className="block text-sm font-bold mb-2" htmlFor="hora">
-                        Hora
-                    </label>
-                    <input
-                        className="bg-main-900 text-white shadow appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
-                        id="hour"
-                        name="hour"
-                        type="Time"
-                        placeholder="Hora"
-                        onChange={handleChange}
-                        value={!params.id ? newTravel.hour : editTravel?.hour}
-                    />
-                </div>
-                <div className="mb-4">
-                    <label className="block text-sm font-bold mb-2" htmlFor="lugares">
-                        Lugares
-                    </label>
-                    <input
-                        className=" bg-main-900 text-white shadow appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
-                        id="places"
-                        name="places"
-                        type="text"
-                        placeholder="Lugares"
-                        onChange={handleChange}
-                        value={params.id ? newTravel.places : editTravel?.places}
-                    />
-                </div>
-                <div className="mb-4">
-                    <label className="block text-sm font-bold mb-2" htmlFor="descripcion">
-                        Descripción
-                    </label>
-                    <input
-                        className=" bg-main-900 text-white shadow appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
-                        id="description"
-                        name="description"
-                        type="text"
-                        placeholder="Descripción"
-                        onChange={handleChange}
-                        value={!params.id ? newTravel.description : editTravel?.description}
-                    />
-                </div>
+                {inputFields.map((field) => (
+                    <div className="mb-4" key={field.name}>
+                        <label className="block text-sm font-bold mb-2" htmlFor={field.name}>
+                            {field.label}
+                        </label>
+                        <input
+                            type={field.type}
+                            id={field.name}
+                            name={field.name}
+                            onChange={handleChange}
+                            value={!id ? newTravel[field.name as FieldName] : editTravel?.[field.name as FieldName]}
+                            className="bg-main-900 text-white shadow appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
+                        />
+                    </div>
+                ))}
 
-                <Button
-                    data={{ text: `${!params.id ? "Crear" : "Modificar"}` }}
-                    events={{ handleClick: handleSubmit }}
-                />
+                <Button data={{ text: `${!id ? "Crear" : "Modificar"}` }} events={{ handleClick: handleSubmit }} />
             </form>
         </div>
     );

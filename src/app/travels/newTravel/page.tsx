111cce5075f46f88ebf29b346e85960d30f38e3b
mussiDev <joakoomussi@gmail.com>
2023-08-21 20:24:00 -0300

    delete call to api

--- src/app/travels/newTravel/page.tsx
+++ src/app/travels/newTravel/page.tsx
@@ -1,16 +1,7 @@
 import Form from "@/src/components/Form/Form";
 import { Suspense } from "react";
 
-async function loadProvinceAndCity() {
-    const response = await fetch("https://apis.datos.gob.ar/georef/api/provincias");
-    if (!response.ok) {
-        throw new Error("Failed to fetch provinces");
-    }
-    const provinces = await response.json();
-    return provinces;
-}
 export default async function NewTravel() {
-    const provinces = await loadProvinceAndCity();
     return (
         <Suspense fallback={<p>Loading...</p>}>
             <Form />
